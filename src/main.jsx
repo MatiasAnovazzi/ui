64ecccc065845afc995c660e29719d8dc6c65482
matitruco <anovazzimatias182@gmail.com>
2025-08-23 21:23:49 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import Ui from './ui.jsx'
+import Ui from './Ui.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
