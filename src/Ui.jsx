ef3455b2bf36893c4e9c67c5d3444939044a7916
matitruco <anovazzimatias182@gmail.com>
2025-08-28 18:17:31 -0300

    optimizacion de columnas

--- src/Ui.jsx
+++ src/Ui.jsx
@@ -1,13 +1,16 @@
 import Nav from './Nav.jsx'
 import Inicio from './inicio.jsx'
 import Section from "./Section.jsx"
+import Columnas from "./columnas.jsx"
 function Ui() {
 
   return (
     <>
       <Nav />
       <Inicio />
-      <Section></Section>
+      <Section />
+      <Columnas/>
+
     </>
   )
 }
---
+++ src/columnas.jsx
@@ -0,0 +1,30 @@
+import "./styles/columnas.css"
+
+function Columnas(){
+    return (
+        <div id="columnas">
+            <h3>¿Como se usa?</h3>
+            <div id="contenedor">
+                <div id="left">
+                <h3>Clientes</h3>
+                <ul id="ul-left">
+                    <li>Te registras como cliente</li>
+                    <li>Buscas el profesional de tu eleccion</li>
+                    <li>Luego de observar la disponibilidad horaria, seleccionas el horario y agendas el turno</li>
+                    <li>Y listo! la app te recuerda tu turno mediante recordatorios por Whatsapp tu turno</li>
+                </ul>
+            </div>
+            <div id="right">
+                <h3>Profesionales</h3>
+                <ul id="ul-right">
+                    <li>Te registras como profesional</li>
+                    <li>Cargas tu actividad y disponibilidad horaria</li>
+                    <li>Y listo! Cuando un cliente cargue un nuevo turno, podes visuarlizarlo desde tu pagina personal</li>
+                </ul>
+            
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Columnas
